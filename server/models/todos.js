d52cc7bcaafa40d2d8160ac83f89f6e658af69a0
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-09-10 13:05:38 +0900

    migration機能でDB接続設定

---
+++ server/models/todos.js
@@ -0,0 +1,18 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const todos = sequelize.define('todos', {
+    taskId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    taskContent: DataTypes.STRING,
+    taskState: DataTypes.STRING
+  }, {
+    underscored: true
+  })
+  todos.associate = function (models) {
+    // associations can be defined here
+  }
+  return todos
+}
